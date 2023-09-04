3f4e77b6a26d6e274be6e5da26bb043243f80466
Kauan Carolino <98748289+KauanCarolino@users.noreply.github.com>
2023-09-04 19:46:43 -0300

    UPDATE do CRUD pronto

--- src/br/com/agenda/dao/ContatoDAO.java
+++ src/br/com/agenda/dao/ContatoDAO.java
@@ -62,7 +62,49 @@ public class ContatoDAO {
 			}
 		} 
 	} 
-
+	
+	public void update(Contato contato) {
+		
+		String sql = "UPDATE contatos SET nome = ?, idade = ?, dataCadastro = ?"
+		+"WHERE id = ?";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			
+			//Criar conexão com o banco
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			//Criar a classe para executar a query
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			//Adiciona valores para atualizar
+			pstm.setString(1, contato.getNome());
+			pstm.setInt(2, contato.getIdade());
+			pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+			
+			//Qual o ID do registro que deseja atualizar
+			pstm.setInt(4, contato.getId());
+			
+			//Executando a query
+			pstm.execute();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstm != null) {
+					pstm.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
     public List<Contato> getContatos() {
         String sql = "SELECT * FROM contatos";
         List<Contato> contatos = new ArrayList<Contato>();
